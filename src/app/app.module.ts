e81d6527f89f986f36e0c42e9c61eeccf6d07ab0
Khaled Mansour <khaledmansour818@gmail.com>
2023-03-15 23:07:44 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AddFormComponent } from './add-form/add-form.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddFormComponent
+    AddFormComponent,
+    UserAuthComponent
   ],
   imports: [
     BrowserModule,
